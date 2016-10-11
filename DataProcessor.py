4cebd2afb0563955056c1de6b82c46b4cce12ab0
arnabdutta <arnab.dutta@number26.de>
2016-10-11 13:09:19 +0200

    memory files for large files

--- DataProcessor.py
+++ DataProcessor.py
@@ -2,18 +2,35 @@
 pre-process all data formats
 """
 import pandas as pd
+import mmap
+import json
 
 
 def json_to_dataframe(json_object):
     """
     takes as input a json file and creates a dataframe object from it
     """
-    # read the entire file into a python array
+    # # read the entire file into a python array
+    # with open(json_object, 'rb') as f:
+    #     data = f.readlines()
+
+    data_json_str = []
+
     with open(json_object, 'rb') as f:
-        data = f.readlines()
+        # Size 0 will read the ENTIRE file into memory!
+        m = mmap.mmap(f.fileno(), 0, access=mmap.PROT_READ)  # File is open read-only
+
+        # Proceed with your code here -- note the file is already in memory
+        # so "readine" here will be as fast as could be
+        data = m.readline()
+        while data:
+            # remove the trailing "\n" from each line
+            # data = map(lambda x: x.rstrip(), data)
+            data_json_str.append(json.loads(data))
+            data = m.readline()
 
     # remove the trailing "\n" from each line
-    data = map(lambda x: x.rstrip(), data)
+    # data = map(lambda x: x.rstrip(), data)
 
     # each element of 'data' is an individual JSON object.
     # i want to convert it into an *array* of JSON objects
@@ -21,6 +38,6 @@ def json_to_dataframe(json_object):
     # basically... add square brackets to the beginning
     # and end, and have all the individual business JSON objects
     # separated by a comma
-    data_json_str = "[" + ','.join(data) + "]"
+    # data_json_str = "[" + ','.join(data) + "]"
 
-    return pd.read_json(data_json_str)
+    return pd.DataFrame(data_json_str)
--- Test.py
+++ Test.py
@@ -2,18 +2,23 @@ import DataProcessor as data_proc
 import Setup as setup
 
 if __name__ == '__main__':
-    # json_file = "/Users/arnabdutta/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_checkin.json"
 
-    df_checkin = data_proc.json_to_dataframe('data/yelp_academic_dataset_checkin.json')
+    # prefix to say where the data is lying
+    prefix = setup.data_path if False else "data/"
 
-    # df_business = data_proc.json_to_dataframe()
-    # df_tip = data_proc.json_to_dataframe()
-    # df_user = data_proc.json_to_dataframe()
-    # df_review = data_proc.json_to_dataframe()
+    # df_checkin = data_proc.json_to_dataframe(prefix + 'yelp_academic_dataset_checkin.json')
+    # print "Checking data loaded {}".format(df_checkin.shape)
+    #
+    # df_business = data_proc.json_to_dataframe(prefix + 'yelp_academic_dataset_business.json')
+    # print "Business data loaded {}".format(df_business.shape)
 
-    print df_checkin.shape
+    # df_tip = data_proc.json_to_dataframe(prefix + 'yelp_academic_dataset_tip.json')
+    # print "Tip data loaded {}".format(df_tip.shape)
 
-    # column_names = j2c.get_superset_of_column_names_from_file(json_file_path=json_file)
-    # j2c.read_and_write_file("world_bank.json", "world_bank.csv", column_names)
+    df_user = data_proc.json_to_dataframe(prefix + 'yelp_academic_dataset_user.json')
+    print "User data loaded {}".format(df_user.shape)
 
+    # df_review = data_proc.json_to_dataframe(prefix + 'yelp_academic_dataset_review.json')
+    # print "Review data loaded {}".format(df_review.shape)
+    
     print("Done..")
